1f9d45f37616f04fa8dbe04a82fb558d3b8bf12f
ackrethomes <ackrethomes@gmail.com>
2026-03-27 10:45:55 -0500

    pricing redirect to no user-new layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { createServerClient } from "@/hooks/supabase/server";
+import { createClient } from "@/hooks/supabase/server";
 import DashboardSidebar from "@/components/dashboard/DashboardSidebar";
 
 export default async function DashboardLayout({
@@ -7,7 +7,7 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createServerClient();
+  const supabase = await createClient();
 
   // Get logged in user
   const {
